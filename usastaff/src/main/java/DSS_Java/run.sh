eda795ace8d8b1ece4d8d2e997322a108f864e2c
Steven Yang <>
2018-02-15 13:59:26 -0500

    fixed jar classpath setting

--- usastaff/src/main/java/DSS_Java/run.sh
+++ usastaff/src/main/java/DSS_Java/run.sh
@@ -19,7 +19,7 @@ echo You MUST edit this file before you can run this application.
 # the Java Development Kit and AXIS 1.4 library location.
 
 
-JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_131.jdk/Contents/Home
+JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_76.jdk/Contents/Home
 JAR_HOME=./lib
 
 # Build the CLASSPATH required
@@ -28,7 +28,7 @@ CLASSPATH=
 #  CLASSPATH="$CLASSPATH:$dir"
 #done
 for jar in axis commons-discovery-0.2 commons-logging-1.1 log4j-1.2.8 jaxrpc \
-	saaj wsdl4j-1.5.1 usastaffdss.jar ; do
+	saaj wsdl4j-1.5.1 usastaffdss ; do
   CLASSPATH="$CLASSPATH:$JAR_HOME/$jar.jar"
 done
 
