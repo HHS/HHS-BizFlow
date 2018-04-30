24839bb300775006fce87510549bf5fd03c51ba1
Steven Yang <>
2018-04-30 11:53:20 -0400

    remove persondirectory which is superseded by biis

--- persondirectory/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd ~/Desktop/locator/build
-ant
-if [ $? -ne 0 ]
-then
-    echo "error happend"
-fi
--- persondirectory/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-CLASS_PATH='/home/user/Desktop/locator'
-
-java -cp "$CLASS_PATH/dist/lib/*:$CLASS_PATH/src/main/resources" com.hhs.persondirectory.HHSPersonDirectoryInterfaceApplication
