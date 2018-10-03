3e41dd921bb21350ebdfcd4ec03070517d047ed6
chrlehman <33495225+chrlehman@users.noreply.github.com>
2018-10-03 16:23:16 -0400

    EHRP 15 min fix for null list

--- ehrp15/dist/EHRP_15min.sh
+++ ehrp15/dist/EHRP_15min.sh
@@ -3,11 +3,12 @@
 #Pulls EHRP data from MARS Drop Box to BizFlow local server
 ############################################################
 
-SCRIPT_HOME=<INSERT directory path for location of getEHRPFiles_sftp.txt>
-LOG_HOME=<INSERT directory path for log files>
-EHRP_FILES=<INSERT directory path for inbound files>
-EHRP_ARCH=<Insert directory path for archived files>
-EHRP_ERROR=<Insert directory path for error files>
+REMOTE_FOLDER=<Insert MARs folder that contains the EHRP files> 
+MARS=<Insert MARS sftp connection string>
+LOG_HOME=<Insert directory that contains log files>
+EHRP_FILES=<Insert directory that contains inbound files>
+EHRP_ARCH=<Insert directory that contains archive files>
+EHRP_ERROR=<Insert directory that contains error files>
 NOW=$(date +"%m-%d-%y-%H-%M-%S-%p")
 CURRDATE=`date`
 LOGFILE=$LOG_HOME/getEHRPFiles_$NOW
@@ -15,39 +16,53 @@ echo "Started script at $CURRDATE" >> $LOGFILE
 echo $CURRDATE > $LOG_HOME/lastRetrieval
 
 #Connect to MARS Drop Box to Fetch Files
-sftp -b $SCRIPT_HOME/getEHRPFiles_sftp.txt <INSERT SFTP CONNECTION STRING FOR MARS DROP BOX>
+cd $EHRP_FILES
+sftp $MARS <<EOF
+        lcd $EHRP_FILES
+        cd $REMOTE_FOLDER
+        mget *.xml
+        rm *.xml
+        quit
+EOF
 if [ $? -ne 0 ]; then
-	echo "EHRP file transfer failed from MARS Drop Box to Ewits Server" >> $LOGFILE
+        echo "EHRP file transfer failed from MARS Drop Box to Ewits Server" >> $LOGFILE
 else
-	echo "EHRP file transfer successful between MARS Drop Box and Ewits Server" >> $LOGFILE
+        echo "EHRP file transfer successful between MARS Drop Box and Ewits Server" >> $LOGFILE
 fi
 
+
 #Change permissions to read/write for inbound files
-if cd $EHRP_FILE; then
-	find -type f -exec chmod 644 *.xml {} \;
+if [ -z "$(ls -A $EHRP_FILES)" ]; then
+        echo "There are no files in the inbound directory" >> $LOGFILE
 else
-	echo "Error in changing permissions for inbound files" >> $LOGFILE
+        cd $EHRP_FILES
+        chmod 644 *.xml
+        echo "Changed permissions for inbound files" >> $LOGFILE
 fi
 
 #Removes Log Files Older than 180 Days
-if cd $LOG_HOME; then
-	find . \( ! -name . -prune \) -type f -prune -mtime +180 -exec rm {} \;
+cd $LOG_HOME
+if [ $? -ne 0 ]; then
+        echo "Error in removing Log Files Older than 180 Days" >> $LOGFILE
 else
-	echo "Error in removing Log Files Older than 180 Days" >> $LOGFILE
+        find . \( ! -name . -prune \) -type f -prune -mtime +180 -exec rm {} \;
+        echo "Removed log files older than 180 days" >> $LOGFILE
 fi
 
 #Removes Archive Files Older than 180 Days
-if cd $EHRP_ARCH; then
-	find . \( ! -name . -prune \) -type f -prune -mtime +180 -exec rm {} \;
+cd $EHRP_ARCH
+if [ $? -ne 0 ]; then
+        echo "Error in removing Archive Files Older than 180 Days" >> $LOGFILE
 else
-	echo "Error in removing Archive Files Older than 180 Days" >> $LOGFILE
+        find . \( ! -name . -prune \) -type f -prune -mtime +180 -exec rm {} \;
+        echo "Removed archive files older than 180 days" >> $LOGFILE
 fi
 
 #Removes Error Files Older than 180 Days
-if cd $EHRP_ERROR; then
-	find . \( ! -name . -prune \) -type f -prune -mtime +180 -exec rm {} \;
+cd $EHRP_ERROR
+if [ $? -ne 0 ]; then
+        echo "Error in removing Error Files Older than 180 Days" >> $LOGFILE
 else
-	echo "Error in removing Error Files Older than 180 Days" >> $LOGFILE
+        find . \( ! -name . -prune \) -type f -prune -mtime +180 -exec rm {} \;
+        echo "Removed error files older than 180 days" >> $LOGFILE
 fi
-
-echo "Finished script at `date`" >> $LOGFILE
