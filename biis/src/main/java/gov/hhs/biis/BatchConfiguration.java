1d0e5e7a9f0a4226085abef43e4a3e44459e864d
chrlehman <33495225+chrlehman@users.noreply.github.com>
2018-10-12 12:27:52 -0400

    Adding fix for batch job serialization issue "java.sql.SQLException: ORA-08177: can't serialize access for this transaction"

--- biis/src/main/java/gov/hhs/biis/BatchConfiguration.java
+++ biis/src/main/java/gov/hhs/biis/BatchConfiguration.java
@@ -15,12 +15,18 @@ import org.springframework.batch.core.configuration.annotation.DefaultBatchConfi
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableBatchProcessing 
@@ -122,6 +128,27 @@ public class BatchConfiguration {
 	@Bean(name="targetJdbcTemplate")
 	public JdbcTemplate targetJdbcTemplate() {
 		return new JdbcTemplate(hhsDataSource);
+	}
+	
+	//***To resolve error: Can't serialize access for this transaction***
+	private JobRepository getJobRepository() throws Exception {
+		JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+		factory.setDataSource(hhsDataSource);
+		factory.setTransactionManager(getTransactionManager());
+		factory.setIsolationLevelForCreate("ISOLATION_READ_COMMITTED");//Added to get rid of ->Caused by: java.sql.SQLException: ORA-08177: can't serialize access for this transaction
+		factory.afterPropertiesSet();
+		return (JobRepository) factory.getObject();
+	}
+
+	private PlatformTransactionManager getTransactionManager() {
+		return new ResourcelessTransactionManager();
+	}
+
+	public JobLauncher getJobLauncher() throws Exception {
+		SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+		jobLauncher.setJobRepository(getJobRepository());
+		jobLauncher.afterPropertiesSet();
+		return jobLauncher;
 	}
 
 }
