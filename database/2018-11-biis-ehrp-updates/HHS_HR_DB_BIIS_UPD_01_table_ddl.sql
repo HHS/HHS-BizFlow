3319e74f74acdec1454f6b5f1c6828b2da7e50d2
chrlehman <33495225+chrlehman@users.noreply.github.com>
2018-10-26 09:34:59 -0400

    Updating column size on of EEOC_OPDIV from varchar2(20) to varchar2(30) for admin code table

---
+++ database/2018-11-biis-ehrp-updates/HHS_HR_DB_BIIS_UPD_01_table_ddl.sql
@@ -0,0 +1,5 @@
+------------------------------------
+--DDL for table ADMINISTRATIVE_CODE
+----------------------------------
+ALTER TABLE HHS_HR.ADMINISTRATIVE_CODE
+MODIFY (EEOC_OPDIV VARCHAR2(30));
