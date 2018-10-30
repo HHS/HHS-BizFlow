7ecb01acbb92c2d80ac3ff8ad39b48a9b7d89d0c
chrlehman <33495225+chrlehman@users.noreply.github.com>
2018-10-30 12:53:01 -0400

    Create index on EMPLOYEE_LOOKUP table

--- database/2018-11-biis-ehrp-updates/HHS_HR_DB_BIIS_UPD_01_table_ddl.sql
+++ database/2018-11-biis-ehrp-updates/HHS_HR_DB_BIIS_UPD_01_table_ddl.sql
@@ -3,3 +3,9 @@
 ----------------------------------
 ALTER TABLE HHS_HR.ADMINISTRATIVE_CODE
 MODIFY (EEOC_OPDIV VARCHAR2(30));
+
+
+---------------------------------
+--Create index on EMPLOYEE_LOOKUP
+---------------------------------
+CREATE INDEX EMPLOYEE_LOOKUP_IDX1 ON HHS_HR.EMPLOYEE_LOOKUP (EMPLID);
