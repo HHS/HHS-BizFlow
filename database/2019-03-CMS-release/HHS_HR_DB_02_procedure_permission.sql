0858dc61d1e38014e2a25ee23d72e6a094f1ec58
prabhjyot.virdi <prabhjyot.virdi@HHSL9YR8PL2.itsc.hhs-itsc.local>
2019-02-19 21:59:01 -0500

    Adding email proc to deployment folder

---
+++ database/2019-03-CMS-release/HHS_HR_DB_02_procedure_permission.sql
@@ -0,0 +1,30 @@
+
+----------------------------------
+-- grant for HHS_HR_RW_ROLE
+----------------------------------
+
+GRANT EXECUTE ON HHS_HR.SP_SEND_MAIL TO HHS_HR_RW_ROLE;
+
+----------------------------------
+-- grant for HHS_HR_DEV_ROLE
+----------------------------------
+
+GRANT EXECUTE ON HHS_HR.SP_SEND_MAIL TO HHS_HR_DEV_ROLE;
+
+----------------------------------
+-- grant for HHS_CMS_HR_RW_ROLE
+----------------------------------
+
+GRANT EXECUTE ON HHS_HR.SP_SEND_MAIL TO HHS_CMS_HR_RW_ROLE;
+
+----------------------------------
+-- grant for HHS_CMS_HR_DEV_ROLE
+----------------------------------
+
+GRANT EXECUTE ON HHS_HR.SP_SEND_MAIL  TO HHS_CMS_HR_DEV_ROLE;
+
+----------------------------------
+-- grant for HHS_CMS_HR
+----------------------------------
+
+GRANT EXECUTE ON HHS_HR.SP_SEND_MAIL  TO HHS_CMS_HR;
