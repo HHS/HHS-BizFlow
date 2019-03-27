ce5ffd5bcdafacc991dbc314849695015da95bae
chrlehman <33495225+chrlehman@users.noreply.github.com>
2019-03-27 16:59:02 -0400

    User Story 290899: Added WGI Due Date and SCD to Employee Lookup

---
+++ database/2019-05-release/HHS_HR_UPD_DB_01_alter_table_ddl.sql
@@ -0,0 +1,10 @@
+-----------------------------------------
+--DDL for table EMPLOYEE_LOOKUP
+-----------------------------------------
+ALTER TABLE
+   HHS_HR.EMPLOYEE_LOOKUP
+ADD
+   (
+    WGI_DUE_DATE      DATE,
+    SCD               DATE
+   );
---
+++ database/2019-05-release/HHS_HR_UPD_DB_02_table_permission.sql
@@ -0,0 +1,10 @@
+
+----------------------------------
+-- grant for HHS_HR_RW_ROLE
+----------------------------------
+GRANT DELETE, INSERT, SELECT, UPDATE ON HHS_HR.EMPLOYEE_LOOKUP TO HHS_HR_RW_ROLE;
+
+----------------------------------
+-- grant for HHS_HR_DEV_ROLE
+----------------------------------
+GRANT DELETE, INSERT, SELECT, UPDATE ON HHS_HR.EMPLOYEE_LOOKUP TO HHS_HR_DEV_ROLE;
