22e6875b5ea4c3e7a52e4f654d61246e9f154dd3
chrlehman <33495225+chrlehman@users.noreply.github.com>
2019-05-09 10:20:17 -0400

    Added index to Time to Offer table

--- database/HHS_HR_DB_USAS_01_dss_table_ddl.sql
+++ database/HHS_HR_DB_USAS_01_dss_table_ddl.sql
@@ -1013,6 +1013,33 @@ CREATE TABLE HHS_HR.AREA_CONSIDERATION
 ---------------------
 
 ------------------------------------
+--DDL for table DSS_TIME_TO_STAFF
+------------------------------------
+CREATE TABLE HHS_HR.DSS_TIME_TO_STAFF
+(
+    STAFFING_ORGANIZATION           VARCHAR2(122),
+    REQUEST_NUMBER                  VARCHAR2(202),
+    REQUEST_CREATE_DATE             DATE,
+    REQUEST_APPROVAL_DATE           DATE,
+    REQUEST_STATUS                  VARCHAR2(1002),
+    REQUEST_TYPE                    VARCHAR2(1002),
+    POSITION_TITLE                  VARCHAR2(202),
+    PAYPLAN_SERIES_GRADE            VARCHAR2(1000),
+    AREA_CONSIDER                   VARCHAR2(24),
+    CERTIFICATE_NUMBER              VARCHAR2(102),
+    CERTIFICATE_SERIES              VARCHAR2(1028),
+    CERTIFICATE_GRADE               VARCHAR2(502),
+    CERTIFICATE_LOCATION            VARCHAR2(2050),
+    CERTIFICATE_ISSUE_DATE          DATE,
+    REVIEW_RETURN_DATE              DATE
+);
+
+----------------------------------------------
+--DDL for index DSS_TIME_TO_STAFF_I1
+----------------------------------------------
+CREATE INDEX HHS_HR.DSS_TIME_TO_STAFF_I1 ON DSS_TIME_TO_STAFF (REQUEST_NUMBER);
+
+------------------------------------
 --DDL for table DSS_TIME_TO_STAFF_STG
 ------------------------------------
 CREATE TABLE HHS_HR.DSS_TIME_TO_STAFF_STG
@@ -1038,6 +1065,36 @@ CREATE TABLE HHS_HR.DSS_TIME_TO_STAFF_STG
 --DDL for index DSS_TIME_TO_STAFF_STG_I1
 ----------------------------------------------
 CREATE INDEX HHS_HR.DSS_TIME_TO_STAFF_STG_I1 ON DSS_TIME_TO_STAFF_STG (REQUEST_NUMBER);
+
+------------------------------------
+--DDL for table DSS_TIME_TO_OFFER
+------------------------------------
+CREATE TABLE HHS_HR.DSS_TIME_TO_OFFER
+(
+    STAFFING_ORGANIZATION           VARCHAR2(122),
+    REQUEST_NUMBER                  VARCHAR2(202),
+    PD_NUMBER                       VARCHAR2(2050),
+    NEW_HIRE_CREATE_DATE            DATE,
+    REQUEST_STATUS                  VARCHAR2(1002),
+    REQUEST_TYPE                    VARCHAR2(1002),
+    POSITION_TITLE                  VARCHAR2(2050),
+    AREA_CONSIDER                   VARCHAR2(24),
+    NEW_HIRE_PAY_PLAN               VARCHAR2(2050),
+    NEW_HIRE_SERIES                 VARCHAR2(2050),
+    NEW_HIRE_GRADE                  VARCHAR2(2050),
+    SEND_TENT_OFFR_CMPL_DATE        DATE,
+    INIT_BKGRND_INVST_DATE          DATE,
+    RCVE_BKGRND_INVST_DATE          DATE,
+    SEND_OFCL_OFFR_CMPL_DATE        DATE,
+    NEW_HIRE_NAME                   VARCHAR2(2050),
+    EOD_DATE                        DATE
+);
+
+----------------------------------------------
+--DDL for index DSS_TIME_TO_OFFER
+----------------------------------------------
+CREATE INDEX HHS_HR.DSS_TIME_TO_OFFER_I1 ON DSS_TIME_TO_OFFER (REQUEST_NUMBER);
+CREATE INDEX HHS_HR.DSS_TIME_TO_OFFER_I2 ON DSS_TIME_TO_OFFER (NEW_HIRE_NAME);
 
 ------------------------------------
 --DDL for table DSS_TIME_TO_OFFER_STG
@@ -1067,6 +1124,7 @@ CREATE TABLE HHS_HR.DSS_TIME_TO_OFFER_STG
 --DDL for index DSS_TIME_TO_OFFER_STG_I1
 ----------------------------------------------
 CREATE INDEX HHS_HR.DSS_TIME_TO_OFFER_STG_I1 ON DSS_TIME_TO_OFFER_STG (REQUEST_NUMBER);
+CREATE INDEX HHS_HR.DSS_TIME_TO_OFFER_STG_I2 ON DSS_TIME_TO_OFFER_STG (NEW_HIRE_NAME);
 
 ---------------------
 --IHS
